503bf686b100b2b345b93aed1744695cf2d8ff73
Eduardo <eduardofonseca0210@gmail.com>
2022-02-09 11:08:14 -0300

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/* Desafio 01*/
+
+var dados = "";
+
+function btn() {
+    dados =  document.getElementById('name').value;
+    document.getElementById('valor').innerHTML = dados;
+
+    dados = document.getElementById('idade').value;
+    document.getElementById('valor2').innerHTML = dados;
+
+    dados = document.getElementById('skills').value;
+    document.getElementById('valor3').innerHTML = dados;
+}
+
+/* Desafio 01*/
