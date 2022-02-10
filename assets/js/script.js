bfa7c8371950d9f163e047da500a855f436e4700
Eduardo <eduardofonseca0210@gmail.com>
2022-02-10 10:01:21 -0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,18 +30,49 @@ function btn() {
 
 /* Desafio 02 -Inicio*/
 
-    function btn2 () {
-    
-    var trocado = document.querySelector('#troco').value;
+function calcular(){
 
-    
+    if(troco.value != ""){
+        var moedaUmReal = 1 * troco.value + 0;
+        var moedaCinq = moedaUmReal * 2;
+        var moedaVin = moedaUmReal * 4;
+        var moedaDez = moedaUmReal * 10;
+        var moedaCinc = moedaUmReal * 20;
 
+        result.innerHTML += "Você vai receber " + moedaUmReal + " moedas de 1 Real de troco<br>";
+        result.innerHTML += "Você vai receber " + moedaCinq + " moedas de 50 Centavos de troco<br>";
+        result.innerHTML += "Você vai receber " + moedaVin + " moedas de 25 Centavos de troco<br>";
+        result.innerHTML += "Você vai receber " + moedaDez + " moedas de 10 Centavos de troco<br>";
+        result.innerHTML += "Você vai receber " + moedaCinc + " moedas de 5 Centavos de troco<br>";
     }
+}
 
 /* Desafio 02 - Fim*/
 
 /* Desafio 03 - Inicio*/
 
+function btn2(){
+    const frase= document.querySelector("#frase").value
+    let arraypalavra= frase.split(" ")
+    arraypalavra=arraypalavra.length
+    let arrayfrase=frase.split(".")
+    for(i in arrayfrase){
+        if(arrayfrase[i]==""){
+            arrayfrase.splice(i,i)
+        }
+    }
+    arrayfrase= arrayfrase.length
+    let letras=frase
+    for(i in frase){
+        letras = letras.replace(" ","")
+        letras = letras.replace(",","")
+        letras = letras.replace(".","")
+    }
+    letras= letras.length
+    const resultado= document.querySelector("#fresultado")
+    fresultado.innerHTML=fresultado.innerHTML +`Esse paragrafo tem ${letras} letras, ${arraypalavra} palavras, e ${arrayfrase} frases.<br>`
+
+}
 
 /* Desafio 03 - Fim*/
 
