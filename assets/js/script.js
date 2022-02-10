adfc7736c0b144e935628c4940611ded92c32438
Eduardo <eduardofonseca0210@gmail.com>
2022-02-09 21:59:09 -0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,62 @@
-/* Desafio 01*/
-
-var dados = "";
+/* Desafio 01 - Inicio*/
 
 function btn() {
-    dados =  document.getElementById('name').value;
-    document.getElementById('valor').innerHTML = dados;
 
-    dados = document.getElementById('idade').value;
-    document.getElementById('valor2').innerHTML = dados;
+    var valor1 = document.getElementById('name').value;
+    var valor2 = document.getElementById('idade').value;
+    var valor3 = document.getElementById('skills').value;
 
-    dados = document.getElementById('skills').value;
-    document.getElementById('valor3').innerHTML = dados;
+    document.querySelector('#resultado').innerHTML = "Prazer em conhecer você " + valor1 + ", você tem " + valor2 + " Anos, e está aprendendo " + valor3 + ".<br>Parabéns pela sua dedicação ^^";
+
+    if (valor1 == "") {
+    document.querySelector('#resultado').innerHTML = "Preencha com os dados";
+    valor1.name.focus();
+    return false;
+    }
+    if (valor2 == "") {
+    document.querySelector('#resultado').innerHTML = "Preencha com os dados";
+    valor2.idade.focus();
+    return false;
+    }
+    if (valor3 == "") {
+    document.querySelector('#resultado').innerHTML = "Preencha com os dados";
+    valor3.skills.focus();
+    return false;
+    }
 }
 
-/* Desafio 01*/
+/* Desafio 01 - Fim*/
+
+
+/* Desafio 02 -Inicio*/
+
+    function btn2 () {
+    
+    var trocado = document.querySelector('#troco').value;
+
+    
+
+    }
+
+/* Desafio 02 - Fim*/
+
+/* Desafio 03 - Inicio*/
+
+
+/* Desafio 03 - Fim*/
+
+/* Desafio Extra - Inicio*/
+
+var captar = "";
+function chamar (){
+    captar = document.getElementById('name').value;
+    document.getElementById('nome').innerHTML = captar;
+
+    captar = document.getElementById('idade').value;
+    document.getElementById('idades').innerHTML = captar;
+
+    captar = document.getElementById('skills').value;
+    document.getElementById('skill').innerHTML = captar;
+}
+
+/* Desafio Extra - Fim*/
